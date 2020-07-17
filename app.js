0280fd447ffc46faca9521324dc31c8b9f16f2db
José Nery <josenerydev@gmail.com>
2020-07-17 20:41:23 -0300

    191. Retrieving Indexes with indexOf() & lastIndexOf()

--- app.js
+++ app.js
@@ -44,10 +44,15 @@
 // const removedElements = hobbies.splice(-2, 1);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 // const storedResults = testResults.slice(2);
 const storedResults = testResults.concat([3.99, 2]);
 
 testResults.push(5.91);
 
 console.log(storedResults, testResults);
+// console.log(testResults.lastIndexOf(1.5));
+console.log(testResults.indexOf(1.5));
+
+const personData = [{ name: 'Max' }, { name: 'Manuel' }];
+console.log(personData.indexOf({ name: 'Manuel' }));
