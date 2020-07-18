de816f1e5d4b3c5ad4761587aabfbe1ff814447d
José Nery <josenerydev@gmail.com>
2020-07-17 21:05:01 -0300

    195. Transforming Data with map()

--- app.js
+++ app.js
@@ -74,17 +74,27 @@
 
 // console.log(maxIndex);
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
-const taxAdjustedPrices = [];
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
 
 // for (const price of prices) {
 //   taxAdjustedPrices.push(price * (1 + tax));
 // }
 
-prices.forEach((price, index, prices) => {
+// prices.forEach((price, index, prices) => {
+//   const priceObj = { index: index, taxAdjPrice: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+
+const taxAdjustedPrices = prices.map((price, index, prices) => {
   const priceObj = { index: index, taxAdjPrice: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
