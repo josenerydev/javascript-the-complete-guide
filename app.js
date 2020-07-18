8df0f45f06ccb74a7adb6c37f618074f4903980e
José Nery <josenerydev@gmail.com>
2020-07-17 23:26:18 -0300

    202. The Spread Operator(...)

--- app.js
+++ app.js
@@ -147,3 +147,23 @@ console.log(transformedData);
 const nameFragments = ['Max', 'Schwarz'];
 const name = nameFragments.join(' ');
 console.log(name);
+
+const copiedNameFragments = [...nameFragments];
+nameFragments.push('Mr');
+console.log(nameFragments, copiedNameFragments);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+
+console.log(Math.min(...prices));
+
+const persons = [{ name: 'Max', age: 30 }, { name: 'Manuel', age: 31 }];
+// const copiedPersons = [...persons.map(person => ({ name: person.name, age: person.age }))];
+const copiedPersons = persons.map(person => ({
+  name: person.name,
+  age: person.age
+}));
+
+persons.push({ name: 'Anna', age: 29 });
+persons[0].age = 31;
+
+console.log(persons, copiedPersons);
